c81379044c1b179baee969c0f068abbb7f4ecd76
Matin Massoudi <matin@massoudi.net>
2020-10-06 15:11:07 -0700

    Implemented methods in AddingMachine.java and added test class

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,25 +2,55 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history = "";
   
+  /**
+   * Default constructor.
+   */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "0";
   }
   
+  /**
+   * Getter method that returns the total.
+   * @return
+   */
   public int getTotal () {
-    return 0;
+    return total;
   }
   
+  /**
+   * Adds passed parameter to total.
+   * @param value
+   */
   public void add (int value) {
+    total += value;
+    history += " + "+ value;
   }
 
+  /**
+   * Subtracts passed parameter to total.
+   * @param value
+   */
   public void subtract (int value) {
+    total -= value;
+    history += " - "+ value;
   }
 
+  /**
+   * toString method.
+   */
+  @Override
   public String toString () {
-    return "";
+    return history;
   }
 
+  /**
+   * Sets total and history to 0.
+   */
   public void clear() {
+    total = 0;
+    history = "0";
   }
 }
---
+++ test.java
@@ -0,0 +1,14 @@
+package cse360assignment02;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AddingMachine test = new AddingMachine();
+		test.add(1);
+		test.subtract(1);
+		System.out.println(test.toString());
+
+	}
+
+}
